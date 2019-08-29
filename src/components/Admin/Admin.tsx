50564b6e031574b050f04471ddc6eefd2f0c1fbb
Ahnna <ahnnaostling@gmail.com>
2019-08-29 16:22:49 +0200

    add axios get and print raw data

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -1,12 +1,49 @@
-
-
 import React from 'react'
 
-    class Admin extends React.Component {  // Should be a stateless functional component
-    render() {
+const axios = require('axios');
+
+// interface IReservationsState {
+//     IReservation: IReservation[]
+// }
+
+// interface IReservation {
+//     id: number,
+//     guests: number,
+//     date: string,
+//     time: number,
+//     name: string,
+//     email: string,
+//     phone: number
+
+// }
+
+interface IState {
+    reservations: any
+}
+
+    class Admin extends React.Component <{}, IState >{  // Should be a stateless functional component
+        constructor(props: any){
+            super(props)
+           this.state = {
+               reservations: []
+           }
+        } 
+        componentDidMount() {
+            axios.get('http://localhost/react-restaurant-booking-backend/reservations.php/')
+            .then((result: any)=> {
+                this.setState({
+                    reservations: result.data
+                })
+                console.log(this.state.reservations)
+            });
+        }
+
+        render() {
+
+    
         return (
             <div>
-                <p>This is the Admin component!</p>
+                <p>{this.state.reservations}</p>
             </div>
         )
     }
