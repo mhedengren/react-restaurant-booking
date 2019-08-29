5117c196029cbf24f0fb953b90a9bb778180d527
mhedengren <melker.hedengren@medieinstitutet.se>
2019-08-29 14:08:04 +0200

    booking form boilerplate, parmis & melker

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -1,13 +1,38 @@
 import React from 'react'
 
-class Booking extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>This is the booking component!</p>
-            </div>
-        )
-    }
+interface IBookingState {
+    numberOfGuest: number
+    date: any
 }
 
-export default Booking
+class BookingForm extends React.Component <{}, IBookingState> {
+    constructor(props:any) {
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event:any) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event:any) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
+  }
+export default BookingForm
