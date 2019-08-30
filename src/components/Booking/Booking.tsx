7c9335e831c1bf30031e3bfd6ba9ceff8204a797
mhedengren <melker.hedengren@medieinstitutet.se>
2019-08-30 13:19:58 +0200

    moment displays correct timestamp

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -1,76 +1,17 @@
 import React from 'react'
-import Calendar from 'react-calendar'
-import moment from 'moment';
+import BookingForm from './BookingForm';
 
-interface IBookingState {
-    numberOfGuests: number;
-    date: Date;
-    time: number;
-}
-
-class BookingForm extends React.Component<{}, IBookingState> {
-  constructor(props: any) {
+class Booking extends React.Component<{}, {}> {
+  constructor(props:any){
     super(props)
-    this.state = {
-      numberOfGuests: 1,
-      date: new Date(),
-      time: 18
-    }
-
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleSubmit(event: any) {
-      console.log(event)
-   let newDate = moment(this.state.date);
-   alert(newDate.format('MMMM Do YYYY, h:mm'))
-  
-   //    alert(this.state.numberOfGuests + this.state.date + this.state.time)
-    event.preventDefault()
-  }
-
-  calendarOnChange = (date:any) => this.setState({ date })
-
-  handleInputChange(event:any) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-
-    this.setState({
-        [name]: value
-      } as any);
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Number of guests:
-          <input
-            name='numberOfGuests'
-            type='number'
-            value={this.state.numberOfGuests}
-            onChange={this.handleInputChange}
-            min='1'
-            max='6'
-          />
-        </label>
-        <label>
-            Time:
-        <select
-          name='time'
-          value={this.state.time}
-          onChange={this.handleInputChange}
-        >
-          <option value='18'>18</option>
-          <option value='21'>21</option>
-        </select>
-        </label>
-        <Calendar onChange={this.calendarOnChange} value={this.state.date} />
-        <input type='submit' value='Submit' />
-      </form>
+  render(){
+    return(
+      <div>
+        <BookingForm />
+      </div>
     )
   }
 }
-export default BookingForm
+export default Booking
--- src/components/Booking/BookingForm.tsx
+++ src/components/Booking/BookingForm.tsx
@@ -1,13 +1,76 @@
 import React from 'react'
+import Calendar from 'react-calendar'
+import moment from 'moment';
 
-class BookingForm extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>This is the booking component!</p>
-            </div>
-        )
-    }
+interface IBookingState {
+    numberOfGuests: number;
+    date: Date;
+    time: number;
 }
 
+class BookingForm extends React.Component<{}, IBookingState> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      numberOfGuests: 1,
+      date: new Date(),
+      time: -1
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(event: any) {
+    console.log(event)
+   let newDate = moment(this.state.date);
+   alert(newDate.format('YYYY-MM-DD'))
+  
+    event.preventDefault()
+  }
+
+  calendarOnChange = (date:any) => this.setState({ date })
+
+  handleInputChange(event:any) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+        [name]: value
+      } as any);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Number of guests:
+          <input
+            name='numberOfGuests'
+            type='number'
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange}
+            min='1'
+            max='6'
+          />
+        </label>
+        <label>
+            Time:
+        <select
+          name='time'
+          value={this.state.time}
+          onChange={this.handleInputChange}
+        >
+          <option value='-1'>Välj tid</option>
+          <option value='18'>18</option>
+          <option value='21'>21</option>
+        </select>
+        </label>
+        <Calendar onChange={this.calendarOnChange} value={this.state.date} />
+        <input type='submit' value='Submit' />
+      </form>
+    )
+  }
+}
 export default BookingForm
