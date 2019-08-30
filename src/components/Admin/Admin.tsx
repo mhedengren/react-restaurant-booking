002c17af10a5204340623042c72fb532af842066
Ahnna <ahnnaostling@gmail.com>
2019-08-30 13:11:23 +0200

    create loop to show admin data in a table

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -1,38 +1,35 @@
-import React from 'react'
+import React from 'react';
+import ReservationsView from './ReservationsView';
 
 const axios = require('axios');
 
-// interface IReservationsState {
-//     IReservation: IReservation[]
-// }
-
-// interface IReservation {
-//     id: number,
-//     guests: number,
-//     date: string,
-//     time: number,
-//     name: string,
-//     email: string,
-//     phone: number
-
-// }
+export interface IReservation {
+    id: number,
+    guests: number,
+    date: string,
+    time: number,
+    name: string,
+    email: string,
+    phone: number
+}
 
 interface IState {
-    reservations: any
+    reservations: IReservation[];
 }
 
     class Admin extends React.Component <{}, IState >{  // Should be a stateless functional component
         constructor(props: any){
-            super(props)
-           this.state = {
-               reservations: []
-           }
+            super(props);
+
+            this.state = {
+                reservations: []
+            }
         } 
         componentDidMount() {
             axios.get('http://localhost/react-restaurant-booking-backend/admin.php/')
             .then((result: any)=> {
                 this.setState({
-                    reservations: result.data
+                    reservations: JSON.parse(result.data)
                 })
                 console.log(this.state.reservations)
             });
@@ -40,12 +37,12 @@ interface IState {
 
         render() {
    
-        return (
-            <div>
-                <p>{[this.state.reservations]}</p>
-            </div>
-        )
-    }
+            return (
+                <ReservationsView
+                    reservations={this.state.reservations}
+                />
+              )
+        }
 }
 
 export default Admin
