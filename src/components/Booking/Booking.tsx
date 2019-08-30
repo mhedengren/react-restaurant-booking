878e92dcb4d82aa8b43b9613da5c177859e7f052
mhedengren <melker.hedengren@medieinstitutet.se>
2019-08-30 10:14:44 +0200

    Added number of guests input and alert it

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -1,25 +1,28 @@
 import React from 'react'
 
 interface IBookingState {
-    numberOfGuest: number
+    numberOfGuests: number
     date: any
 }
 
 class BookingForm extends React.Component <{}, IBookingState> {
     constructor(props:any) {
       super(props);
-      this.state = {value: ''};
+      this.state = {
+        numberOfGuests: 0,
+        date: 0
+        };
   
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
   
     handleChange(event:any) {
-      this.setState({value: event.target.value});
+      this.setState({numberOfGuests: event.target.value});
     }
   
     handleSubmit(event:any) {
-      alert('A name was submitted: ' + this.state.value);
+      alert('This is how many who wants to book: ' + this.state.numberOfGuests);
       event.preventDefault();
     }
   
@@ -28,7 +31,7 @@ class BookingForm extends React.Component <{}, IBookingState> {
         <form onSubmit={this.handleSubmit}>
           <label>
             Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <input type="number" value={this.state.numberOfGuests} onChange={this.handleChange} max="6" />
           </label>
           <input type="submit" value="Submit" />
         </form>
