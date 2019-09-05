52c47c1dd4b2b90e4999c07ea5c1ba99c6f1c972
Parmis-K <parmis.kamali@gmail.com>
2019-09-05 16:27:09 +0200

    lift-state-up

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -3,26 +3,41 @@ import BookingForm from './BookingForm';
 import ContactForm from "./ContactForm";
 import moment from 'moment'
 
-interface IBookingState {
-  guests: number,
 
+
+//Interface för hur hela bokningen ser ut
+interface IBookingState {
+  guests: number;
+  date: string,
+  time: number;
 }
+
 
 class Booking extends React.Component<{}, {}> {
   constructor(props:any){
     super(props)
+
+    this.getFirstFormInfo = this.getFirstFormInfo.bind(this)
   }
 
-  awesomeFunction(numberOfGuests: number, date: moment.Moment, time:number ){
+  //getFirstFormInfo är funktionen som vi skickar ned till BookingForm via props
+  //Sätta state/Lyfta ut state från vår andra komponent
+  //hämtar värdena 
+  getFirstFormInfo(numberOfGuests: number, date: string, time:number){
     this.setState({
-      guests: numberOfGuests
+      guests: numberOfGuests,
+      date: date,
+      time: time
     })
   }
 
+
+   
   render(){
     return (
       <div>
-        <BookingForm sendToBooking={this.awesomeFunction} />
+        {/* Namnger propsen och skickar ner funktionen*/}
+ <  BookingForm sendToBooking={this.getFirstFormInfo} />
         <div>
           <ContactForm />
         </div>
