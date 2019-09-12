08750e70dc65fee111299bcecaf6c68b46c3c924
mhedengren <melker.hedengren@medieinstitutet.se>
2019-09-12 16:21:58 +0200

    minor indentation and comments

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -47,7 +47,7 @@ class Booking extends React.Component<{}, IBookingState> {
 
   //Get values from the booking form.
   getBookingFormValues(numberOfGuests: number, date: string, time: number) {
-    // If time selection dropdown is on "Choose time" it wont display the contact form.
+    // If time selection dropdown is on "Pick a time" it wont display the contact form.
     if (time == 1){
       this.setState({
         showContactForm: false
@@ -85,6 +85,7 @@ class Booking extends React.Component<{}, IBookingState> {
         }) 
         return false
     } 
+    //This doesn't work for some reason, it will always be false :( Validation error in contact form?
     if (!this.state.contactFormValid){
       return false
     }
